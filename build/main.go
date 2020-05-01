53231d57f60905a5fc4d5f82b76be6d8fb4411a1
Michael Chandra <canzinzzzide@yahoo.co.id>
2020-05-01 14:07:09 +0700

    add gin/gin-gonic

--- build/main.go
+++ build/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"os"
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main () {
-	fmt.Println("Initial Commit")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/michaelchandrag/warung-pintar
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.6.2 // indirect
